19f01e9c73cffc38d7788d46e9175446a7a9ad4d
nerudaj <xnerud01@stud.fit.vutbr.cz>
2018-02-23 12:49:43 +0100

    UI overhaul
    UI has been reworked to match the SCGUI1 counterpart. Output format was
    simplified since the GUI always displays both CSV and pretty output.

--- frontend/src/app/security-cloud/sc-dbqry/sc-dbqry.component.ts
+++ frontend/src/app/security-cloud/sc-dbqry/sc-dbqry.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operator/map';
 import { debounceTime } from 'rxjs/operator/debounceTime';
 import { distinctUntilChanged } from 'rxjs/operator/distinctUntilChanged';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 
 // Local modules
 import { ProfileMap } from '../modules/Profile';
@@ -15,6 +16,7 @@ import { AppConfig } from '../modules/AppConfig';
 // Services
 import { ScDbqryService } from './sc-dbqry.service';
 import { ScDbqryFilterComponent } from './sc-dbqry-filter/sc-dbqry-filter.component';
+import { ScDbqryIplookupComponent } from './sc-dbqry-iplookup/sc-dbqry-iplookup.component';
 
 const BTN_QUERY_READY = {
     label: 'Start query',
@@ -43,6 +45,7 @@ export class ScDbqryComponent implements OnInit, OnChanges {
     @Input() filter: string; ///< Filter textarea model
     @Input() channels: ChannelSettings[] = null;
     @ViewChild('DbqryFilter') filterComponent : ScDbqryFilterComponent;
+    @ViewChild('IPLookup') iplookupComponent : ScDbqryIplookupComponent;
 
     limitto = [ ///< LimitTo dropdown model
         {value: '-l 10', desc: '10 records'},
@@ -57,23 +60,42 @@ export class ScDbqryComponent implements OnInit, OnChanges {
         {value: '-l 10000', desc: '10000 records'}
     ];
     selectedLimit = '-l 10'; ///< Selection index to limitto
-    //fields = null; ///< Aggregation model
-    fieldsAggregate = null;
+    
+    // Orderby
     fieldsOrderby = null;
-    fieldsPrint = null;
-    selectedOrderBy = '';
+    selectedOrderBy = 'Nothing';
     selectedOrderDir = '';
+    
+    // Aggregate
+    fieldsAggregate = null;
     selectedAggregation = '';
-    selectedOutputFmt = 'pretty';
+    
+    // Output
+    customfields = false;
+    fieldsMultiselectSettings = {
+        enableSearch: true,
+        checkedStyle: 'fontawesome',
+        buttonClasses: 'btn btn-default btn-block',
+        dynamicTitleMaxItems: 10,
+        displayAllSelectedText: true
+    };
+    fieldsMultiselectOptions = null;
+    fieldsMultiselectSelected = [0, 3, 4, 5, 6, 7, 8, 10];
+    
+    // Output processing
+    selectedOutputProcessing = 'kmb';
     nosummary = false; ///< No summary checkbox model
+    
+    // Custom Options
     customOpts: string = null;
-    fancy: boolean; ///< Whether output will be displayed as a table or simple text
 
+    // Querying
     btnQuery = BTN_QUERY_READY;
     instanceID: string = null; ///< Identifier of the browser tab
     progressStyle; ///< Styler of progress bar
     timer = null; ///< Handle for setInterval trigger
 
+    // Query output
     toPrint_command: string = null; ///< Result text model
     toPrint_stdout = null; ///< Result text model
     toPrint_stdoutRaw = null; ///< Unformatted text model
@@ -135,6 +157,7 @@ export class ScDbqryComponent implements OnInit, OnChanges {
     btnQueryClick(event: any) {
         if (this.btnQuery.querying) {
             const filter: string = this.getFilter();
+            let timeArg : string = this.getArgTime();
             let args: string;
 
             if (this.customOpts == null) {
@@ -195,11 +218,29 @@ export class ScDbqryComponent implements OnInit, OnChanges {
     }
 
     /**
+     *  @brief Format graph time selection as fdistdumpt time argument
+     *  
+     *  @return Formatted string usable as fdistdump argument
+     */
+    getArgTime() : string {
+        let result : string;
+        
+        if (this.sel.bgn === this.sel.end) {
+            result = '-t ' + this.asUnixTimestamp(this.sel.bgn);
+        }
+        else {
+            result = '-T ' + this.asUnixTimestamp(this.sel.bgn) + '#' + this.asUnixTimestamp(this.sel.end);
+        }
+        
+        return result;
+    }
+
+    /**
      *  @brief Get options from quick select/custom textarea boxes
      *
      *  @return Formatted string
      *
-     *  @details The options are specifying limit, time range, aggregation, orderBy, order direction,
+     *  @details The options are specifying limit, aggregation, orderBy, order direction,
      *  and output format.
      *
      *  @note !!! IMPORTANT !!! fdistdump has three formatting options: pretty, long and csv. First
@@ -207,24 +248,14 @@ export class ScDbqryComponent implements OnInit, OnChanges {
      *  Csv is the barbone option with all conversion disabled (but they can be enabled manually).
      *  Since I need to break the fdistdump output down to render it within a table, to place down
      *  buttons, etc, I needed a little hack which maybe can confuse advanced users.
-     *  First off, pretty/long was merged to a single option - pretty and second, flask backend tests
-     *  the arguments for presence of pretty/long option and if it is there, then it is replaced
-     *  with csv format and conversions enabled. When returned back to frontend, I can easily parse
-     *  this csv and display it. But there maybe will be users who want raw text output, that can
-     *  be copypasted somewhere. For that reason when the mode is set to csv, the output will be
-     *  as raw as possible.
+     *  Frontend claims to request pretty format but in reality it requests for csv format with various
+     *  prettyfying arguments on. It then loads the request and displays the output as a prettyfied table
+     *  as well as a original CSV.
      */
     getArgs(): string {
         let result: string;
 
-        if (this.sel.bgn === this.sel.end) {
-            result = '-t ' + this.asUnixTimestamp(this.sel.bgn);
-        }
-        else {
-            result = '-T ' + this.asUnixTimestamp(this.sel.bgn) + '#' + this.asUnixTimestamp(this.sel.end);
-        }
-
-        result += ' ' + this.selectedLimit;
+        result = this.selectedLimit;
 
         if (this.selectedAggregation !== '') {
             result += ' -a ';
@@ -248,28 +279,32 @@ export class ScDbqryComponent implements OnInit, OnChanges {
             result += ' -o ' + this.selectedOrderBy;
 
             if (this.selectedOrderDir !== '') {
-                result += "#" + this.selectedOrderDir;
+                result += '#¨' + this.selectedOrderDir;
             }
         }
 
-        if (this.selectedOutputFmt === 'pretty') {
+        if (this.customfields && this.fieldsMultiselectSelected.length > 0) {
+            result += ' --fields ' + this.fieldsMultiselectOptions[this.fieldsMultiselectSelected[0]].name;
+            for (let i = 1; i < this.fieldsMultiselectSelected.length; i++) {
+                result += ',' + this.fieldsMultiselectOptions[this.fieldsMultiselectSelected[i]].name;
+            }
+        }
+
+        // In reality, backend sends csv output format but user sees it as pretty
+        if (this.selectedOutputProcessing === 'raw') {
+            result += ' --output-format=csv';
+        }
+        else if (this.selectedOutputProcessing === 'kmb') {
             result += ' --output-format=pretty --output-ts-conv="%F %R"';
         }
-        else {
-            result += ' --output-format=csv';
+        else if (this.selectedOutputProcessing === 'kimibi') {
+            result += ' --output-format=pretty --output-volume-conv=binary-prefix --output-ts-conv="%F %R"';
         }
 
         result += ' --output-items=r';
         if (!this.nosummary) {
             result += ',m';
         }
-
-        /*
-        NOTE: Following code is deprecated. Local time on the server is not the same as the client's
-        local time so performing localtime conversion by fdistdump server-side makes no sense.
-        if (this.config.useLocalTime) {
-            result += ' --output-ts-localtime';
-        }*/
 
         return result;
     }
@@ -352,11 +387,8 @@ export class ScDbqryComponent implements OnInit, OnChanges {
     processQueryResult(data: Object) {
         this.toPrint_stdout = data['out'];
 
-        // Test csv mode and set fancy flag
-        this.fancy = ((this.toPrint_command.indexOf('--output-format=pretty') !== -1)
-            || (this.toPrint_command.indexOf('--output-format=long') !== -1));
-
-        if (this.fancy && data['out'] !== '') {
+        // stdout data must be now processed from CSV to pretty as well
+        if (data['out'] !== '') {
             this.toPrint_stdoutRaw = data['out'];
 
             const sections = data['out'].split('\n\n');
@@ -374,9 +406,6 @@ export class ScDbqryComponent implements OnInit, OnChanges {
                     this.toPrint_stdout[i].data[p - 1] = aux[p].split(',');
                 }
             }
-        }
-        else {
-            this.toPrint_stdout = data['out'];
         }
 
         this.toPrint_stderr = data['err'];
@@ -437,7 +466,13 @@ export class ScDbqryComponent implements OnInit, OnChanges {
         */
         this.fieldsAggregate = data['aggregate'];
         this.fieldsOrderby = data['orderby'];
-        this.fieldsPrint = data['print'];
+        this.fieldsMultiselectOptions = new Array<IMultiSelectOption>();
+        let cnt = 0;
+        for (let item of data['print']) {
+            this.fieldsMultiselectOptions.push({id: cnt, name: item['name']});
+            cnt++;
+        }
+        
         this.selectedOrderBy = this.fieldsOrderby[0].name;
     }
 
